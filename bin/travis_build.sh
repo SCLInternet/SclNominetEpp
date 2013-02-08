171f7d95e5bf62cc977a06e1a8d0fe3cb59cc140
Tom Oram <tom@scl.co.uk>
2013-02-08 10:54:30 +0000

    Using php fixer for cs

--- bin/travis_build.sh
+++ bin/travis_build.sh
@@ -6,6 +6,9 @@ PHPUNIT=$?
 vendor/bin/phpcs --standard=psr2 src
 PHPCS=$?
 
+php php-cs-fixer.phar fix -v --dry-run --level=psr2 ./src/
+PHPFX=$?
+
 vendor/bin/phpmd src text codesize
 PHPMD=$?
 
@@ -19,7 +22,11 @@ if [ "$PHPCS" -ne "0" ]; then
     echo "**** Coding standards failed"
     EXIT=1
 fi
-if [ "$PHPCS" -ne "0" ]; then
+if [ "$PHPFX" -ne "0" ]; then
+    echo "**** Coding standards failed"
+    EXIT=1
+fi
+if [ "$PHPMD" -ne "0" ]; then
     echo "**** Mess detection failed"
     EXIT=1
 fi
