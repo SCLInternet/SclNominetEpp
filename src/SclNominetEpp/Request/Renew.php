4afe5168324ef31c3c9790132a67f9687305ddba
James Wade <jpswade@gmail.com>
2021-09-05 09:15:48 +0100

    fix: cleanup, modernise and improve interfacing for renewals, allows you to specify period

--- src/SclNominetEpp/Request/Renew.php
+++ src/SclNominetEpp/Request/Renew.php
@@ -1,22 +1,25 @@
 <?php
 /**
- * Contains the nominet Renew request class definition.
- *
- * @author Tom Oram <tom@scl.co.uk>
+ * Contains the Nominet Renew request class definition.
  */
 
 namespace SclNominetEpp\Request;
 
-use SclNominetEpp\Response\Renew as RenewResponse;
 use SclNominetEpp\Request;
+use SclNominetEpp\Response\Renew as RenewResponse;
+use SimpleXMLElement;
 
 /**
  * This class build the XML for a Nominet EPP renew command.
- *
- * @author Tom Oram <tom@scl.co.uk>
  */
 class Renew extends Request
 {
+    /** @const int Default renewal period, two years (depending on unit). */
+    const DEFAULT_PERIOD = 2;
+
+    /** @const int Default renewal unit, years. */
+    const DEFAULT_UNIT = 'y';
+
     /**
      * The domain name.
      *
@@ -29,50 +32,82 @@ class Renew extends Request
      *
      * @var string
      */
-    protected $expDate;
+    protected $currentExpiryDate;
+
+    /** @var int The period to register for. */
+    protected $period = self::DEFAULT_PERIOD;
+
+    /** @var string The unit used for the period. */
+    protected $unit = self::DEFAULT_UNIT;
 
     /**
      * Tells the parent class what the action of this request is.
+     * @param string|null $domain
      */
-    public function __construct()
+    public function __construct(string $domain = null)
     {
         parent::__construct('renew', new RenewResponse());
+        $this->setDomain($domain);
     }
 
     /**
-     * Set the date
+     * Set the domain
      *
      * @param string $domain
-     * @param DateTime $expDate
+     * @param \DateTimeInterface|null $currentExpiryDate
      * @return \SclNominetEpp\Request\Renew
      */
-    public function setDomain($domain, $expDate)
+    public function setDomain(string $domain, \DateTimeInterface $currentExpiryDate = null)
     {
         $this->domain = $domain;
-        $this->expDate = $expDate;
+
+        if ($currentExpiryDate) {
+            $this->setDate($currentExpiryDate);
+        }
 
         return $this;
     }
 
     /**
-     * (non-PHPdoc)
-     * @see SclNominetEpp\Request.AbstractRequest::addContent()
+     * Set the date
      *
+     * @param \DateTimeInterface $currentExpiryDate
+     * @return \SclNominetEpp\Request\Renew
+     */
+    public function setDate(\DateTimeInterface $currentExpiryDate)
+    {
+        $this->currentExpiryDate = $currentExpiryDate;
+        return $this;
+    }
+
+    /**
+     * Set the period
+     *
+     * @param int $period
+     * @return \SclNominetEpp\Request\Renew
+     */
+    public function setPeriod(int $period)
+    {
+        $this->period = $period;
+        return $this;
+    }
+
+    /**
+     * (non-PHPdoc)
      * @param SimpleXMLElement $xml
+     * @see Request.AbstractRequest::addContent()
+     *
      */
     protected function addContent(SimpleXMLElement $xml)
     {
-        $domainNS  = 'urn:ietf:params:xml:ns:domain-1.0';
+        $domainNS = 'urn:ietf:params:xml:ns:domain-1.0';
         $domainXSI = $domainNS . ' domain-1.0.xsd';
-
-        //$domainNS  = 'urn:ietf:params:xml:ns:domain-1.0';
-        //$domainXSI = 'urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd';
 
         $domainRenew = $xml->addChild('domain:renew', '', $domainNS);
         $domainRenew->addAttribute('xsi:schemaLocation', $domainXSI, self::XSI_NAMESPACE);
         $domainRenew->addChild('name', $this->domain);
-        $domainRenew->addChild('curExpDate', $this->expDate);
-        $period = $domainRenew->addChild('period', 2);
-        $period->addAttribute('unit', 'y');
+        $domainRenew->addChild('curExpDate', $this->currentExpiryDate);
+        $period = $domainRenew->addChild('period', $this->period);
+        $period->addAttribute('unit', $this->unit);
     }
 }
