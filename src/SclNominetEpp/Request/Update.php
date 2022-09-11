e8f2e57dfa0d22dfb36c76d97f2b2e7f35055e66
James Wade <jpswade@gmail.com>
2022-09-11 11:16:18 +0100

    fix: make more oopy

--- src/SclNominetEpp/Request/Update.php
+++ src/SclNominetEpp/Request/Update.php
@@ -8,26 +8,42 @@ use SclNominetEpp\Request\Update\Helper\DomainCompareHelper;
 
 class Update
 {
+    /**
+     * @var Domain
+     */
+    private $domain;
+
+    /**
+     * @var Domain
+     */
+    private $currentDomain;
+
+    /**
+     * @var Update\Domain
+     */
+    private $request;
+
     public function __invoke(Domain $domain, Domain $currentDomain): Update\Domain
     {
-        $request = new Update\Domain($domain->getName());
-        $request->setDomain($domain);
+        $this->domain = $domain;
+        $this->currentDomain = $currentDomain;
+        $this->request = new Update\Domain($domain->getName());
+        $this->request->setDomain($domain);
 
-        $currentNameservers = $currentDomain->getNameservers();
-        $currentContacts = $currentDomain->getContacts();
-        $newNameservers = $domain->getNameservers();
-        $newContacts = $domain->getContacts();
+        $this->handleNameservers();
 
-        $addContacts = array_uintersect(
-            $newContacts,
-            $currentContacts,
-            [DomainCompareHelper::class, 'compare']
-        );
-        $removeContacts = array_uintersect(
-            $currentContacts,
-            $newContacts,
-            [DomainCompareHelper::class, 'compare']
-        );
+        $this->handleContacts();
+
+        $this->handleExtra();
+
+        return $this->request;
+    }
+
+    protected function handleNameservers(): void
+    {
+        $currentNameservers = $this->currentDomain->getNameservers();
+        $newNameservers = $this->domain->getNameservers();
+
         $addNameservers = array_uintersect(
             $newNameservers,
             $currentNameservers,
@@ -41,57 +57,75 @@ class Update
 
         if (!empty($addNameservers)) {
             foreach ($addNameservers as $nameserver) {
-                $request->add(new Update\Field\DomainNameserver($nameserver->getHostName()));
-            }
-        }
-
-        if (!empty($addContacts)) {
-            foreach ($addContacts as $type => $contact) {
-                $request->add(new Update\Field\DomainContact($contact->getId(), $type));
+                $this->request->add(new Update\Field\DomainNameserver($nameserver->getHostName()));
             }
         }
 
         if (!empty($removeNameservers)) {
             foreach ($removeNameservers as $nameserver) {
-                $request->remove(new Update\Field\DomainNameserver($nameserver->getHostName()));
+                $this->request->remove(new Update\Field\DomainNameserver($nameserver->getHostName()));
+            }
+        }
+
+        if (empty($this->domain->getNameservers())) {
+            $this->request->add(new Update\Field\Status('Payment overdue.', Nominet::STATUS_CLIENT_HOLD));
+        }
+    }
+
+    protected function handleContacts(): void
+    {
+        $currentContacts = $this->currentDomain->getContacts();
+        $newContacts = $this->domain->getContacts();
+
+        $addContacts = array_uintersect(
+            $newContacts,
+            $currentContacts,
+            [DomainCompareHelper::class, 'compare']
+        );
+        $removeContacts = array_uintersect(
+            $currentContacts,
+            $newContacts,
+            [DomainCompareHelper::class, 'compare']
+        );
+
+        if (!empty($addContacts)) {
+            foreach ($addContacts as $type => $contact) {
+                $this->request->add(new Update\Field\DomainContact($contact->getId(), $type));
             }
         }
 
         if (!empty($removeContacts)) {
             foreach ($removeContacts as $type => $contact) {
-                $request->remove(new Update\Field\DomainContact($contact->getId(), $type));
+                $this->request->remove(new Update\Field\DomainContact($contact->getId(), $type));
             }
         }
+    }
 
-        if (empty($domain->getNameservers())) {
-            $request->add(new Update\Field\Status('Payment overdue.', Nominet::STATUS_CLIENT_HOLD));
+    protected function handleExtra(): void
+    {
+        if ($this->domain->getRegistrant() !== null &&
+            $this->domain->getRegistrant() != $this->currentDomain->getRegistrant()) {
+            $this->request->changeRegistrant($this->domain->getRegistrant());
         }
 
-        if ($domain->getRegistrant() !== null &&
-            $domain->getRegistrant() != $currentDomain->getRegistrant()) {
-            $request->changeRegistrant($domain->getRegistrant());
+        if ($this->domain->getPassword() !== null &&
+            $this->domain->getPassword() != $this->currentDomain->getPassword()) {
+            $this->request->changePassword($this->domain->getPassword());
         }
 
-        if ($domain->getPassword() !== null &&
-            $domain->getPassword() != $currentDomain->getPassword()) {
-            $request->changePassword($domain->getPassword());
+        if ($this->domain->getAutoBill() !== null &&
+            $this->domain->getAutoBill() != $this->currentDomain->getAutoBill()) {
+            $this->request->setAutoBill($this->domain->getAutoBill());
         }
 
-        if ($domain->getAutoBill() !== null &&
-            $domain->getAutoBill() != $currentDomain->getAutoBill()) {
-            $request->setAutoBill($domain->getAutoBill());
+        if ($this->domain->getNextBill() !== null &&
+            $this->domain->getNextBill() != $this->currentDomain->getNextBill()) {
+            $this->request->setNextBill($this->domain->getNextBill());
         }
 
-        if ($domain->getNextBill() !== null &&
-            $domain->getNextBill() != $currentDomain->getNextBill()) {
-            $request->setNextBill($domain->getNextBill());
+        if ($this->domain->getNotes() !== null &&
+            $this->domain->getNotes() != $this->currentDomain->getNotes()) {
+            $this->request->setNotes($this->domain->getNotes());
         }
-
-        if ($domain->getNotes() !== null &&
-            $domain->getNotes() != $currentDomain->getNotes()) {
-            $request->setNotes($domain->getNotes());
-        }
-
-        return $request;
     }
 }
