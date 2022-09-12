43e820c029ec626403ea5379ec6ccd7330c0341f
James Wade <jpswade@gmail.com>
2022-09-12 18:25:46 +0100

    fix: HostName parameter "ns1.example.com." is not the fully qualified name (coming back from the info)

--- src/SclNominetEpp/Nameserver.php
+++ src/SclNominetEpp/Nameserver.php
@@ -69,15 +69,6 @@ class Nameserver
         if (filter_var($hostName, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME) === false) {
             throw new InvalidArgumentException(sprintf('HostName parameter "%s" is invalid', $hostName));
         }
-
-        /**
-         * "A <domain:hostObj> element contains the fully qualified name of a known name server host object."
-         * @see https://www.rfc-editor.org/rfc/rfc5731#section-1.1
-         */
-        if ($hostName !== rtrim($hostName, '.')) {
-            $message = sprintf('HostName parameter "%s" is not the fully qualified name', $hostName);
-            throw new InvalidArgumentException($message);
-        }
         $this->hostName = $hostName;
     }
 
