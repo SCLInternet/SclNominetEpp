c13b930610ff234ce3586b743168b3748af4f228
James Wade <jpswade@gmail.com>
2022-09-12 18:27:47 +0100

    fix: at the point of building the xml, remove the trailing dot

--- src/SclNominetEpp/Request/Update/Field/DomainNameserver.php
+++ src/SclNominetEpp/Request/Update/Field/DomainNameserver.php
@@ -9,7 +9,7 @@ use SimpleXMLElement;
  */
 class DomainNameserver implements UpdateFieldInterface
 {
-    private $nameserver;
+    private string $nameserver;
 
     public function __construct(string $nameserver)
     {
@@ -21,6 +21,11 @@ class DomainNameserver implements UpdateFieldInterface
 
     public function fieldXml(SimpleXMLElement $xml, string $namespace = null)
     {
-        $xml->addChild('hostObj', $this->nameserver, $namespace);
+        /**
+         * "A <domain:hostObj> element contains the fully qualified name of a known name server host object."
+         * @see https://www.rfc-editor.org/rfc/rfc5731#section-1.1
+         */
+        $hostName = rtrim($this->nameserver, '.');
+        $xml->addChild('hostObj', $hostName, $namespace);
     }
 }
