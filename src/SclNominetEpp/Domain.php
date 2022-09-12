8595be3c384f9a10fa596fc442e7f66213d33846
James Wade <jpswade@gmail.com>
2022-09-12 16:14:59 +0100

    fix: cleanup + validation

--- src/SclNominetEpp/Domain.php
+++ src/SclNominetEpp/Domain.php
@@ -15,117 +15,93 @@ class Domain
 
     /**
      * Domain name
-     *
-     * @var string
      */
-    private $name;
+    private string $name;
 
     /**
      * Registration Period
-     *
-     * @var int
      */
-    private $period = self::REGISTRATION_PERIOD;
+    private int $period = self::REGISTRATION_PERIOD;
 
     /**
      * The Person, Company or Entity who owns or holds a domain name.
-     *
-     * @var ?string
      */
-    private $registrant;
+    private ?string $registrant;
 
     /**
      * All the contacts of the registered domain.
      *
      * @var Contact[]
      */
-    private $contacts = [];
+    private array $contacts = [];
 
     /**
      * All the nameservers of the registered domain.
      *
      * @var Nameserver[]
      */
-    private $nameservers = [];
+    private array $nameservers = [];
 
     /**
      * The identifier of the sponsoring client.
      * Specified in the Nominet EPP as "clID"
-     *
-     * @var string
      */
-    private $clientID;
+    private string $clientID;
 
     /**
      * The identifier of the client that created the domain object.
      * Specified in the Nominet EPP as "crID"
-     *
-     * @var string
      */
-    private $creatorID;
+    private string $creatorID;
 
     /**
      * The date and time of domain object creation.
      * Specified in the Nominet EPP as "crDate"
-     *
-     * @var DateTime
      */
-    private $created;
+    private DateTime $created;
 
     /**
      * The date and time identifying the end of the domain object's registration period.
      * Specified in the Nominet EPP as "exDate"
-     *
-     * @var DateTime
      */
-    private $expired;
+    private DateTime $expired;
 
     /**
      * The identifier of the client that last updated the domain object.
      * This variable MUST be null if the domain has never been modified.
      * (could be a name and email address or the value submitted from the <clTRID> element if created by EPP)
-     *
-     * @var string
      */
-    private $upID = null;
+    private ?string $upID = null;
 
     /**
      *
      * The date and time of the most recent domain-object modification, formatted as: YYYYMMDD.
      * This variable MUST be null if the domain object has never been modified.
-     *
-     * @var DateTime
      */
-    private $upDate = null;
+    private ?DateTime $upDate = null;
 
     /**
      * If first-bill is not set or set to "th", the registration
      * invoice will be sent to the registrar,
      * setting this to "bc" will instead invoice the customer at the non-member registration rate.
-     *
-     * @var string
      */
-    private $firstBill = self::BILL_REGISTRAR;
+    private string $firstBill = self::BILL_REGISTRAR;
 
     /**
      * If recur-bill is not set or set to "th" invoices for renewals
      * will be sent to the registrar,
      * setting this to "bc" will instead invoice the customer at the non-member renewal rate
      * (the auto-bill and next-bill fields will also be cleared).
-     *
-     * @var string
      */
-    private $recurBill = self::BILL_REGISTRAR;
+    private string $recurBill = self::BILL_REGISTRAR;
 
     /**
      * The number of days before expiry you wish to automatically renew a domain name.
      * Values between 1-182.
      * This field can be cleared by setting the default value of 0.
      * Auto-bill cannot be set if next-bill, recur-bill or renew-not-required are set.
-     *
-     * @var int
      */
-    private $autoBill;
+    private int $autoBill;
 
     /**
      * The number of days before expiry you wish to automatically renew a domain name.
@@ -133,41 +109,25 @@ class Domain
      * Values between 1 and 182, indicating how many days before expiry you wish to renew the domain name.
      * This field can be cleared by setting the default value of 0.
      * Next-bill cannot be set if auto-bill, recur-bill or renew-not-required are set.
-     *
-     * @var int
      */
-    private $nextBill;
+    private int $nextBill;
 
     /**
      * Domain's current registration status
-     *
-     * @var string
      */
-    private $regStatus;
+    private string $regStatus;
 
     /**
      * Miscellaneous information relating to the domain name.
      *
      * @var ?string[]
      */
-    private $notes;
+    private ?array $notes;
 
     /**
      * Password
-     *
-     * @var string
      */
-    private $password;
-
-    public function getRegistrant(): ?string
-    {
-        return $this->registrant;
-    }
-
-    public function setRegistrant(?string $registrant)
-    {
-        $this->registrant = $registrant;
-    }
+    private string $password;
 
     /**
      * Set add $contact to array of contacts
@@ -178,17 +138,7 @@ class Domain
     }
 
     /**
-     * @return Contact[]
-     */
-    public function getContacts(): array
-    {
-        return $this->contacts;
-    }
-
-    /**
      * Remove $contact from the array of contacts if it already exists.
-     *
-     * @param Contact $contact
      */
     public function removeContact(Contact $contact)
     {
@@ -205,23 +155,100 @@ class Domain
     }
 
     /**
+     * Remove $nameserver from the array of nameservers if it already exists.
+     */
+    public function removeNameserver(Nameserver $nameserver)
+    {
+        $arrayKey = array_search($nameserver, $this->nameservers);
+        unset($this->nameservers[$arrayKey]);
+    }
+
+    public function __toString()
+    {
+        return $this->getName();
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        if (filter_var($name, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME) === false) {
+            throw new InvalidArgumentException(sprintf('Name parameter "%s" is invalid', $name));
+        }
+        $this->name = $name;
+    }
+
+    public function __toArray(): array
+    {
+        $data = [];
+        $data['name'] = $this->getName();
+        $data['period'] = $this->getPeriod();
+        $data['registrant'] = $this->getRegistrant();
+        $data['contacts'] = $this->getContacts();
+        $data['nameservers'] = $this->getNameservers();
+        $data['clientID'] = $this->getClientID();
+        $data['creatorID'] = $this->getCreatorID();
+        $data['created'] = $this->getCreated();
+        $data['expired'] = $this->getExpired();
+        $data['upID'] = $this->getUpID();
+        $data['upDate'] = $this->getUpDate();
+        $data['firstBill'] = $this->getFirstBill();
+        $data['recurBill'] = $this->getRecurBill();
+        $data['autoBill'] = $this->getAutoBill();
+        $data['nextBill'] = $this->getNextBill();
+        $data['regStatus'] = $this->getRegStatus();
+        $data['notes'] = $this->getNotes();
+        $data['password'] = $this->getPassword();
+        return $data;
+    }
+
+    public function getPeriod(): int
+    {
+        return $this->period;
+    }
+
+    /**
+     * Set the value of period
+     */
+    public function setPeriod(int $period): Domain
+    {
+        if ($period < self::REGISTRATION_PERIOD) {
+            $message = sprintf("Invalid period %d, must be greater than %d", $period, self::REGISTRATION_PERIOD);
+            throw new InvalidArgumentException($message);
+        }
+        $this->period = $period;
+
+        return $this;
+    }
+
+    public function getRegistrant(): ?string
+    {
+        return $this->registrant;
+    }
+
+    public function setRegistrant(?string $registrant)
+    {
+        $this->registrant = $registrant;
+    }
+
+    /**
+     * @return Contact[]
+     */
+    public function getContacts(): array
+    {
+        return $this->contacts;
+    }
+
+    /**
      * Get the array of nameservers
      * @return Nameserver[]
      */
     public function getNameservers(): array
     {
         return $this->nameservers;
-    }
-
-    /**
-     * Remove $nameserver from the array of nameservers if it already exists.
-     *
-     * @param Nameserver $nameserver
-     */
-    public function removeNameserver(Nameserver $nameserver)
-    {
-        $arrayKey = array_search($nameserver, $this->nameservers);
-        unset($this->nameservers[$arrayKey]);
     }
 
     /**
@@ -293,7 +320,7 @@ class Domain
 
     public function setUpDate(DateTime $upDate)
     {
-        $this->upDate = $upDate->format(DateTimeInterface::ATOM);
+        $this->upDate = $upDate;
     }
 
     public function getFirstBill(): string
@@ -305,6 +332,18 @@ class Domain
     {
         $this->checkBill($firstBill);
         $this->firstBill = $firstBill;
+    }
+
+    protected function checkBill(string $bill): void
+    {
+        if ($bill === '') {
+            return;
+        }
+        if (in_array($bill, self::BILLS) === false) {
+            $options = implode(', ', self::BILLS);
+            $message = sprintf("Invalid bill '%s', must one of '%s'", $bill, $options);
+            throw new InvalidArgumentException($message);
+        }
     }
 
     public function getRecurBill(): string
@@ -329,7 +368,7 @@ class Domain
      */
     public function setAutoBill($autoBill)
     {
-        $this->autoBill = (int) $autoBill;
+        $this->autoBill = (int)$autoBill;
     }
 
     public function getNextBill(): ?int
@@ -343,7 +382,7 @@ class Domain
      */
     public function setNextBill($nextBill)
     {
-        $this->nextBill = (int) $nextBill;
+        $this->nextBill = (int)$nextBill;
     }
 
     public function getRegStatus(): string
@@ -377,75 +416,5 @@ class Domain
     public function setPassword(string $password)
     {
         $this->password = $password;
-    }
-
-    public function __toString()
-    {
-        return $this->getName();
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name)
-    {
-        $this->name = $name;
-    }
-
-    public function getPeriod(): int
-    {
-        return $this->period;
-    }
-
-    /**
-     * Set the value of period
-     */
-    public function setPeriod(int $period): Domain
-    {
-        if ($period < self::REGISTRATION_PERIOD) {
-            $message = sprintf("Invalid period %d, must be greater than %d", $period, self::REGISTRATION_PERIOD);
-            throw new InvalidArgumentException($message);
-        }
-        $this->period = $period;
-
-        return $this;
-    }
-
-    protected function checkBill(string $bill): void
-    {
-        if ($bill === '') {
-            return;
-        }
-        if (in_array($bill, self::BILLS) === false) {
-            $options = implode(', ', self::BILLS);
-            $message = sprintf("Invalid bill '%s', must one of '%s'", $bill, $options);
-            throw new InvalidArgumentException($message);
-        }
-    }
-
-    public function __toArray(): array
-    {
-        $data = [];
-        $data['name'] = $this->getName();
-        $data['period'] = $this->getPeriod();
-        $data['registrant'] = $this->getRegistrant();
-        $data['contacts'] = $this->getContacts();
-        $data['nameservers'] = $this->getNameservers();
-        $data['clientID'] = $this->getClientID();
-        $data['creatorID'] = $this->getCreatorID();
-        $data['created'] = $this->getCreated();
-        $data['expired'] = $this->getExpired();
-        $data['upID'] = $this->getUpID();
-        $data['upDate'] = $this->getUpDate();
-        $data['firstBill'] = $this->getFirstBill();
-        $data['recurBill'] = $this->getRecurBill();
-        $data['autoBill'] = $this->getAutoBill();
-        $data['nextBill'] = $this->getNextBill();
-        $data['regStatus'] = $this->getRegStatus();
-        $data['notes'] = $this->getNotes();
-        $data['password'] = $this->getPassword();
-        return $data;
     }
 }
