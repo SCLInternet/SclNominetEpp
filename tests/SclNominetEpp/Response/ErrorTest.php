1341edaeced63f70737081fc775883b180b62a9c
James Wade <jpswade@gmail.com>
2022-09-13 20:02:41 +0100

    fix: add test for error

---
+++ tests/SclNominetEpp/Response/ErrorTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace SclNominetEpp\Response;
+
+use PHPUnit\Framework\TestCase;
+use SclNominetEpp\Response;
+use SimpleXMLElement;
+
+/**
+ * response error epp command test.
+ */
+class ErrorTest extends TestCase
+{
+    protected Response $request;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp(): void
+    {
+        $this->request = new Response();
+    }
+
+    public function testError()
+    {
+        $filename = __DIR__ . '/' . pathinfo(__FILE__, PATHINFO_FILENAME) . '.xml';
+        $xml = file_get_contents($filename);
+        $this->request->init($xml);
+        $this->assertEquals(Response::ERROR_COMMAND_SYNTAX, $this->request->code());
+        $this->assertEquals('Command syntax error', $this->request->message());
+        $this->assertInstanceOf(SimpleXMLElement::class, $this->request->data());
+        var_dump($this->request->data());
+        $this->assertEquals(false, $this->request->success());
+    }
+}
